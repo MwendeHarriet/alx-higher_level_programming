656fddabdc5eafbfef354d959a3d51d98595c8ca
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-08 15:44:41 -0700

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        print("{:c}".format(i), end='')
-    else:
-        print("{:c}".format(i - 32), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
 print()
