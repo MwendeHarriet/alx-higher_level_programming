770309865d0efe5566c2a2901109b615aa4414af
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-08 14:22:09 -0700

    isupper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for alphabet in str:
-        print("{:c}".format(ord(alphabet) & 223), end='')
+def uppercase(s):
+    for char in s:
+        if char == ' ':
+            print(' ', end="")
+        elif ord('a') <= ord(char) <= ord('z'):
+            print(chr(ord(char) & 223), end="")
+        else:
+            print(char, end="")
     print()
