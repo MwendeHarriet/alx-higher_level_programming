5a553e49e2a3cbb2d1cd4967174004cb1e3c5d5e
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-11 06:13:15 -0700

    my_calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,23 +8,22 @@ def main():
         print("Usage:", sys.argv[0], "<a> <operator> <b>")
         sys.exit(1)
 
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
-
-if operator == "+":
-    result = add(a, b)
-elif operator == "-":
-    result = sub(a, b)
-elif operator == "*":
-    result = mul(a, b)
-elif operator == "/":
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-    print(f"{a} {operator} {b} = {result}")
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+        print(f"{a} {operator} {b} = {result}")
 
 
 if __name__ == "__main__":
