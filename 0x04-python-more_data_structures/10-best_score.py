c1966f1a426b2d10144846c9ecb091871d0edbd2
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-16 07:03:02 -0700

    best_score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or not a_dictionary:
+        return None
+
+    biggest_key = max(a_dictionary, key=a_dictionary.get)
+    return biggest_key
