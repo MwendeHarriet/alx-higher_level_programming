4ad6b972975515bd16e5463258c1828826d195f3
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-29 06:58:41 -0700

    added documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,18 +3,30 @@
 
 
 class Square:
-    """the class square"""
+    """the class square/represents a square
+    Attributes:
+        __size (intt): Size of square.
+    """
+
     def __init__(self, size=0):
-        """initialises a Square with its parameter size/
-        the constructor/creates an instance of an object(class)
-        Raises TypeError ValueError
-        Attribute:__size (int)/size of square"""
+        """
+        Initialises a Square with its parameters size
+        Args:
+            size (int) the size of the square
+        Raises:
+            TypeError & ValueError-If size is not an integer &
+            if size is less than 0 respectivrly
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """public instance method that returns the current square area"""
-            return (self.__size * self.__size)
+    def area(self):
+        """
+        Calculate the area of the square
+        Returns:
+            Current area of the square(int)
+        """
+        return self.__size ** 2
