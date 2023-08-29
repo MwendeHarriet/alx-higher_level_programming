19bd7a5665d38c9c9947efec0a3ac0f52841dd11
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-29 06:03:29 -0700

    class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class Square module that defines a square"""
+
+
+class Square:
+    """the class square"""
+    def __init__(self, size=0):
+        """initialises a Square with its parameter size/
+        the constructor/creates an instance of an object(class)"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
