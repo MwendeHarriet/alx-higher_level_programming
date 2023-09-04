b95e1b0cf2c3586fe4e794c87867e03ec1b9c765
MwendeHarriet <mwendeharriet@gmail.com>
2023-09-04 11:46:42 -0700

    I haven't finished the last task

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines/solves the N queens problem."""
+
+
+import sys
+
+
+def initialise_chessboard(n):
+    """I will go to bed now. So tired.
+        Tomorrow is another day."""
