cd5eb29aeced6a34b83a39ff79ca8f4c2d9fc61c
MwendeHarriet <mwendeharriet@gmail.com>
2023-09-21 04:43:57 -0700

    tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""The Base """
-import csv
+"""Defines Base class"""
 import json
+import csv
 import turtle
 
 
 class Base:
-    """ Base Class """
+    """Base class body.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Initializes base """
+        """Initialize Id in a constructor
+        """
         if id is not None:
             self.id = id
         else:
@@ -19,116 +22,110 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        """ convert result to Json
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ save json to file """
+        """Save list object to JSON
+        """
         filename = cls.__name__ + ".json"
-        obj_lisst = []
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
 
-        if list_objs is not None:
-            for obj in list_objs:
-                obj_lisst.append(obj.to_dictionary())
-
-        with open(filename, mode='w', encoding='utf-8') as ifile:
-            ifile.write(cls.to_json_string(obj_lisst))
-
+    @staticmethod
     def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
         """
-        Converts JSON to a lisst of dicts.
-        Args:
-            json_string (str): JSON (str) rep of a list.
-        Returns:
-            list: (of dictionaries).
-        """
-        if json_string is None or json_string == "":
+        if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """Return a class instantiated from a dictionary of attributes.
         """
-        Creates an instance using a dict of attr.
-        """
-        if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummy = cls(1)
-        else:
-            return None
-        dummy.update(**dictionary)
-        return dummy
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
 
     @classmethod
     def load_from_file(cls):
-        """Loads inst from JSON ifile."""
-        filename = cls.__name__ + ".json"
+        """Return a list of classes instantiated from a file of JSON  string.
+        """
+        filename = str(cls.__name__) + ".json"
         try:
-            with open(filename, "r") as ifile:
-                json_dataa = ifile.read()
-                lisst_dicts = cls.from_json_string(json_dataa)
-                instances = [cls.create(**dic) for dic in lisst_dicts]
-                return instances
-        except FileNotFoundError:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
             return []
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """ save to csv """
-        if list_objs is None or len(list_objs) == 0:
-            dataa = []
-        else:
-            dataa = [obj.to_csv() for obj in list_objs]
-
+        """Write the CSV serialization of a list of objects to a file.
+        """
         filename = cls.__name__ + ".csv"
-        with open(filename, mode='w', newline='') as ifile:
-            writer = csv.writer(ifile)
-            for r in dataa:
-                writer.writerow(r)
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
 
     @classmethod
     def load_from_file_csv(cls):
-        """ load from csv """
+        """Return a list of class instantiated from a CSV file.
+        """
         filename = cls.__name__ + ".csv"
         try:
-            with open(filename, mode='r', newline='') as ifile:
-                reader = csv.reader(ifile)
-                dataa = [list(map(int, row)) for row in reader]
-            return [cls.create_from_csv(row) for row in dataa]
-        except FileNotFoundError:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
             return []
 
     @staticmethod
-    def draw(list_rectangles, list_squares):
-        """ Turtle pyt"""
-        scrin = turtle.Screen()
-        scrin.bgcolor("white")
+    def draw(cls, list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+        """
+        window = turtle.Screen()
+        pen = turtle.Pen()
+        figures = list_rectangles + list_squares
 
-        draw = turtle.Turtle()
-        draw.speed(1)
+        for fig in figures:
+            pen.up()
+            pen.goto(fig.x, fig.y)
+            pen.down()
+            pen.forward(fig.width)
+            pen.right(90)
+            pen.forward(fig.height)
+            pen.right(90)
+            pen.forward(fig.width)
+            pen.right(90)
+            pen.forward(fig.height)
+            pen.right(90)
 
-        draw.color("blue")
-        for rec in list_rectangles:
-            draw.penup()
-            draw.goto(rec.x, rec.y)
-            draw.pendown()
-            for y in range(2):
-                draw.forward(rec.width)
-                draw.left(90)
-                draw.forward(rec.height)
-                draw.left(90)
-
-        draw.color("green")
-        for sqr in list_squares:
-            draw.penup()
-            draw.goto(sqr.x, sqr.y)
-            draw.pendown()
-            for y in range(4):
-                draw.forward(sqr.size)
-                draw.left(90)
-
-        turtle.exitonclick()
+        window.exitonclick()
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,115 +1,134 @@
 #!/usr/bin/python3
-""" Rec Class """
+"""Rectangle class implement Base class """
 
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Rectangle class, (inherits) from Base.
-    Attributes:
-        _width (int): Private width attr.
-        _height (int): Private height att.
-        _x (int): Private x attr.
-        _y (int): Private y attr.
+    """ rectangle class implement Base class
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initialize Rectangle instance.
-        Args:
-            width (int): rec width.
-            height: rectangle Height.
-            x, y (int, opt): x, y-coordinate of the rectangle's position.
-                Defaults to 0.
-            id (int, opt): Unique identifier for the obj.
-                Defaults to None.
-        """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
-        """Getter for d width att."""
+        """
+            Returning private attribute (__width)
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for d width att."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """
+            Setting private attribute (__width)
+        """
+        self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Getter for d height attr."""
+        """
+            Returning private attribute (___height)
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for d height attr."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        """
+            Setting private attribute (__height)
+        """
+        self.setter_validation("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """Getter for d x attr."""
+        """
+            Returning private attribute (__x)
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for d x attr."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        """
+            Setting private attribute (__x)
+        """
+        self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """Getter for d y attr."""
+        """
+            Returning private attribute (__y)
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for d y attr."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        """
+            Setting private attribute (__y)
+        """
+        self.setter_validation("y", value)
         self.__y = value
 
     def area(self):
-        """ Returns d area val of the Rec instance. """
-        return self.__width * self.__height
+        """
+            Returns the area of the rectangle (height * width)
+        """
+        return (self.height * self.width)
 
     def display(self):
-        """ Update the Rec using # chars """
-        for i in range(self.__height):
-            print("#" * self.__width)
+        """
+            Prints to stdout the representation of the rectangle
+        """
+        rectangle = ""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+        print(rectangle, end="")
 
     def update(self, *args, **kwargs):
-        """ Update d Rec attrs with args in order """
-        if args and len(args) > 0:
-            attr_lisst = ["id", "width", "height", "x", "y"]
-            for i, val in enumerate(args):
-                if i < len(attr_lisst):
-                    setattr(self, attr_lisst[i], val)
-        else:
+        """
+            Updates the arguments props in the class
+        """
+        if len(args) == 0:
             for key, val in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, val)
+                self.__setattr__(key, val)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
+
+    def to_dictionary(self):
+        """
+            Returns a dictionary representation of this class
+        """
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
+
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
 
     def __str__(self):
-        """ Return str rep of d Rec """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)
