40ef040323905a0aa92245fefedb8d9717a4f0c5
MwendeHarriet <mwendeharriet@gmail.com>
2023-10-17 10:50:40 -0700

    advanced

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,12 @@
+-- script converts hbtn_0c_0 database to UTF8 (utf8mb4 collate utf8mb4_unicode_ci) in MySQL server
+-- alter database
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+-- alter table
+USE hbtn_0c_0;
+ALTER TABLE
+    first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- script displays average temp(F) by city by temperature in descending order
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
