b043e7a07b4946fa51d71526a670465d581bd0ba
MwendeHarriet <mwendeharriet@gmail.com>
2023-10-17 10:56:19 -0700

    temp of cities in descending order

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- script displays top 3 cities temp in July and August by temp in descending order
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- script displays max temp for each state ordered by state name.
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
