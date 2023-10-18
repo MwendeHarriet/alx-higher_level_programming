1ab582f67f8d6be7f165ac68cdddf54cbb58fa41
MwendeHarriet <mwendeharriet@gmail.com>
2023-10-18 10:46:59 -0700

    correction of 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,2 +1,11 @@
 -- script creates database hbtn_0d_usa and table cities therein in the MySQL server
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+-- create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create tables
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+)
