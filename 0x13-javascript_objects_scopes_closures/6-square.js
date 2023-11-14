1034e8ae3a76ea5ac058fee48de448a821e8268f
MwendeHarriet <mwendeharriet@gmail.com>
2023-11-14 10:18:22 -0800

    creates method charprint

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const MySquare = require('./5-square');
+
+class Square extends MySquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let x = 0; x < this.height; x++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
