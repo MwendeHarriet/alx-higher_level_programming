802d1ef22b0d400c414a02619100805d0aae4d75
MwendeHarriet <mwendeharriet@gmail.com>
2023-11-14 10:27:50 -0800

    occurences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let occurence = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      occurence += 1;
+    }
+  }
+  return (occurence);
+};
